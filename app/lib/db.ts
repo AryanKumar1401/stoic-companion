fea3373ce7e8edd2e88c133881e2b68b4131099c
Aryan Kumar <aryank1401@gmail.com>
2025-01-07 20:06:56 +0530

    integrated with AWS RDS database

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,6 +2,9 @@ import {Pool} from 'pg';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // Required for AWS RDS SSL connection
+    },
 })
 
 export default pool;
